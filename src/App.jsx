94f988f6954717c026bc440ec18adcb66ab7e643
abasiofon-sendan <abasiofon135@gmail.com>
2026-08-08 16:30:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import Profile from "./pages/Profile"
+import Experience from "./pages/Experience"
+function App() {
+
+
+  return (
+    <>
+<Navbar />
+<Profile />
+<Experience />
+    </>
+  )
+}
+
+export default App
